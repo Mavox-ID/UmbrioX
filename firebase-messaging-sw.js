2c8e3a6bfe0dc5c1cb2138b90bf53a5be5804bb1
Ye-A! <markd.voznyuk@gmail.com>
2025-08-03 21:52:23 +0300

    Firebase MUS

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,24 @@
-// firebase-messaging-sw.js
-importScripts("https://www.gstatic.com/firebasejs/9.24.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.24.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/9.24.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.24.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyA0jTDzGLMdh7UAh6VE07k2KYbV_ftQnQA",
   authDomain: "umbriox-df07e.firebaseapp.com",
   projectId: "umbriox-df07e",
+  storageBucket: "umbriox-df07e.firebasestorage.app",
   messagingSenderId: "239289752920",
-  appId: "1:239289752920:web:0cef4c6d1ecaa60c3c192d",
-  measurementId: "G-F10JP8XXYC"
+  appId: "1:239289752920:web:0cef4c6d1ecaa60c3c192d"
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Получено фоновое сообщение ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/ico.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
