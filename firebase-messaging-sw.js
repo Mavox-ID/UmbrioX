7e2cc95c929b2a2c26336bfafccaf41355bf309b
Ye-A! <markd.voznyuk@gmail.com>
2025-08-06 11:32:17 +0300

    Bug fixes
    Fixing all possible bugs for good operation of devices, also fixing 404 site and improving it, as well as fixing the error with the rocket, stars and animations, as well as everything related to JETTON, blockchain and pools

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,12 @@
-importScripts('https://www.gstatic.com/firebasejs/9.24.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.24.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyA0jTDzGLMdh7UAh6VE07k2KYbV_ftQnQA",
   authDomain: "umbriox-df07e.firebaseapp.com",
   projectId: "umbriox-df07e",
-  storageBucket: "umbriox-df07e.firebasestorage.app",
   messagingSenderId: "239289752920",
-  appId: "1:239289752920:web:0cef4c6d1ecaa60c3c192d"
+  appId: "1:239289752920:web:0cef4c6d1ecaa60c3c192d",
 });
 
 const messaging = firebase.messaging();
